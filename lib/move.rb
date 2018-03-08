f7cd521cf624d14e00011c3bef3411c10a627d0b
Leann Kim <leannminakim@gmail.com>
2018-03-08 02:41:40 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,6 +1,7 @@
-board = [" ", " ", " ", " "," ", " ", " ", " ", " "]
 
-def display_board(board, index)
+
+def display_board(board = [" ", " ", " ", " "," ", " ", " ", " ", " "]
+)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
